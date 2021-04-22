0b872f5061464a51fec9ceedcb95fed8291ada0f
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-04-22 19:42:43 +0200

    Added info

--- 08_context.js
+++ 08_context.js
@@ -2,82 +2,110 @@
 /* Context (определяет как функция была вызвана, и постоянно указывает на ключевое слово this)
 -------------------------------------------------------------------------------*/
 
-// const person = {
-//     surname: "Stark",
-//     knows: function(what, name){
-//         console.log(`You know ${what}, ${name} ${this.surname}`);
-//     }
-// }
 
-// const john = {
-//     surname: "Snow"
-// }
-
-// person.knows("everything", "Bran")
-
-// // Context transfer
-// // NOTE function in reality is an object
-// person.knows.call(john, "nothing", "John")
-// person.knows.apply(john, ["nothing", "John"])
+/*-----------------------------------------------------------------------------*/
+/* Bind, Call, Apply
+-------------------------------------------------------------------------------*/
+// NOTE function in reality is an object
 
 
-// person.knows.bind(john, "nothing", "John")()
-// // or...
-// const bound = person.knows.bind(john, "nothing", "John")
-// bound()
+const club1 = {
+    club: "Manchester",
+    num: 7,
+    player: function(name) {
+        console.log(`${this.club}'s player, ${name}, number ${this.num}.`);
+    }
+}
 
-
-
-// function Person(name, age){
-//     this.name = name,
-//     this.age = age
-
-//     console.log(this);
-// }
-
-// const elena = new Person("Elena", 20)
+const club2 = {
+    club: "Madrid",
+    num: 9
+}
 
 
 /*-----------------------------------------------------------------------------*/
-/* Explicit Context 
+/* Bind это способ передачи функции другому обьекту (при этом this будет указывать на новый обьект)
 -------------------------------------------------------------------------------*/
 
-// function logTHIS(){
-//     console.log(this);
-// }
+// club1.player.bind(club2, "Ronaldo")()
+const transfer = club1.player.bind(club2, "Ronaldo")
+transfer()
 
-// let obj = {
-//     num: 42
-// }
+/*-----------------------------------------------------------------------------*/
 
-// logTHIS.call(obj)
-// logTHIS.apply(obj)
-// logTHIS.bind(obj)()
+/*-----------------------------------------------------------------------------*/
+/* Call is very similar to bind
+-------------------------------------------------------------------------------*/
+// Differences:
+// 1. Executes the function it was called upon right away.
+// 2. The call() method does not make a copy of the function it is being called on. (!!!)
+
+club1.player.call(club2, "Ronaldo")
+
+/*-----------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------*/
+/* Apply is exact same as call 
+-------------------------------------------------------------------------------*/
+// Differences:
+// 1. Expects an array of parameters.
+
+club1.player.apply(club2, ["Ronaldo"])
+
+/*-----------------------------------------------------------------------------*/
+
+
+
 
 
 /*-----------------------------------------------------------------------------*/
-/* Implicit Context
+/* Passing a function to an object
 -------------------------------------------------------------------------------*/
 
-// const animal = {
-//     legs: 8,
-//     logTHIS: function(){
-//         console.log(this);
-//     }
-// }
 
-// animal.logTHIS()
+const myself = {
+    name: "Dimon"
+}
+
+function me() {
+    console.log(`My name is ${this.name}.`);
+}
+
+me.bind(myself)()
 
 
 /*-----------------------------------------------------------------------------*/
 /* Arrow function Context
 -------------------------------------------------------------------------------*/
+// NOTE arrow function does not create its own context
+// https://www.freecodecamp.org/news/learn-es6-the-dope-way-part-ii-arrow-functions-and-the-this-keyword-381ac7a32881/
 
-function Cat(color){
-    this.color = color
-    console.log("This", this);
-    // Arrow function does not create its own context
-    ( () => console.log("Arrow this", this) )()
+
+// Wrong
+const arrow = {
+    name: "Big fat arrow",
+    log: () => {
+        console.log(this.name); 
+        // undefined BECAUSE ‘this’, always references the owner of the function it is in, 
+        // for this case — since it is now out of scope — the window/global object.
+    }
 }
+arrow.log()
 
-new Cat("red")
+// Right
+const arrow2 = {
+    name: "Big fat arrow2",
+    log() {
+        console.log(this.name);
+        // undefined BECAUSE ‘this’, always references the owner of the function it is in, 
+        // for this case — since it is now out of scope — the window/global object.
+    }
+}
+arrow2.log()
+
+
+
+
+
+
+
--- 10_prototypes.js
+++ 10_prototypes.js
@@ -1,43 +1,93 @@
 /*-----------------------------------------------------------------------------*/
 /* Objects and prototypes
 -------------------------------------------------------------------------------*/
+// https://medium.com/backticks-tildes/javascript-prototypes-ee46810e4866
 
 // __proto__
 // equals
 // Object.getPrototypeOf()
 
-// A class in ES5
 function Cat(name, color){
     this.name = name
     this.color = color
 }
 
+// Прототип это как отдельный обьект куда можно складывать свойства и методы
 Cat.prototype.voice = function(){
     console.log(`Cat ${this.name} says meow`);
 }
 
 const cat = new Cat("Dimon", "white")
+
+console.log(cat); // object, has only initial properties
+
+// Getting prototypes object
+console.log(typeof cat.__proto__); // object
+console.log(Cat.prototype); // same as above
+
+// Constructor function (method of an object)
+console.log(cat.constructor); // ???
+
 cat.voice()
 
+// When an object gets a request for a property that it does not have, its prototype
+// will be searched for the property, then the prototype’s prototype, and so on.So who 
+// is the prototype of an object ? It is the great ancestral prototype, the entity behind 
+// almost all objects, Object.prototype.Many objects don’t directly have Object.prototype 
+// as their prototype, but instead have another object that provides a different set of 
+// default properties.Functions derive from Function.prototype, and arrays derive from 
+// Array.prototype and so on.
 
-// !!!!
-// console.log(Cat.prototype);
-// console.log(cat);
-// console.log(cat.__proto__);
-// console.log(cat.constructor);
+
+
+/*-----------------------------------------------------------------------------*/
+/* Why prototypes are important
+-------------------------------------------------------------------------------*/
+
+
+let protoRabbit = {
+    color: 'grey',
+    speak(line) {
+        console.log(`The ${this.type} rabbit says ${line}`);
+    }
+};
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "assassin";
+killerRabbit.speak("SKREEEE!");
+
+// The method speak in the code above is inefficient because if you were to create multiple 
+// rabbit objects, you would have the same function written everywhere and that’s where 
+// prototype and constructor functions really come in.
+
+
+function ProtoRabbit(color, words, type) {
+    this.color = color;
+    this.words = words;
+    this.type = type;
+};
+ProtoRabbit.prototype.getColor = function () {
+    return this.color;
+}
+ProtoRabbit.prototype.speak = function () {
+    console.log(`The ${this.type} rabbit says ${this.words}`);
+}
+
+let killerRabbit = new ProtoRabbit('grey', 'SKREEEEE!', 'assassin');
+killerRabbit.speak();
+
 
 
 /*-----------------------------------------------------------------------------*/
 /* Есть свойства прототипа и есть собственные (у обьекта)
 -------------------------------------------------------------------------------*/
 
-function Person(){}
+// function Person(){}
 
-Person.prototype.legs = 2;
-Person.prototype.skin = "white"
+// Person.prototype.legs = 2;
+// Person.prototype.skin = "white"
 
-const person = new Person()
-person.name = "Dimon"
+// const person = new Person()
+// person.name = "Dimon"
 
 // console.log("skin" in person); // true 
 // console.log(person.name); // Dimon
@@ -47,5 +97,5 @@ person.name = "Dimon"
 /* Как узнать где свойства прототипа, а где собственные
 -------------------------------------------------------------------------------*/
 
-console.log(person.hasOwnProperty("name"));
-console.log(person.hasOwnProperty("skin"));
+// console.log(person.hasOwnProperty("name"));
+// console.log(person.hasOwnProperty("skin"));
